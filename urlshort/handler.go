1a4952ec1fa9fe3f8b0be14941d7b56c0b8ebd73
seeker815 <ephemeral972@gmail.com>
2019-05-18 18:27:38 +0530

    Made the type def outside the scope of func

--- urlshort/handler.go
+++ urlshort/handler.go
@@ -40,10 +40,6 @@ func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.Handl
 // See MapHandler to create a similar http.HandlerFunc via
 // a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
-	type pathMap struct {
-		Path string `yaml:"path"`
-		URL  string `yaml:"url"`
-	}
 
 	type pms []pathMap
 
@@ -62,4 +58,9 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 
 	return MapHandler(pathsToUrls, fallback), nil
 
+}
+
+type pathMap struct {
+	Path string `yaml:"path"`
+	URL  string `yaml:"url"`
 }
