bb2ca834449d62d26f0d3f257490de510c3fb542
seeker815 <ephemeral972@gmail.com>
2020-04-01 14:08:27 +0530

    Added parsing for task id and rm command support

--- task/cmd/rm.go
+++ task/cmd/rm.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"strconv"
 
+	"github.com/seeker815/gophercises/task/db"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +13,33 @@ var rmCmd = &cobra.Command{
 	Use:   "rm",
 	Short: "rm deletes a task instead of completing it",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("rm called")
+		var ids []int
+		// ensure user invokes the taskId
+		for _, arg := range args {
+			id, err := strconv.Atoi(arg)
+			if err != nil {
+				fmt.Println("Failed to parse the input for taskID ")
+				return
+			}
+			ids = append(ids, id)
+		}
+
+		if len(ids) == 0 {
+			fmt.Println("No available tasks in the list to remove")
+			return
+		}
+		// Delete the task from bucket
+		for _, id := range ids {
+			if id <= 0 {
+				fmt.Println("Invalid task number:", id)
+				continue
+			}
+			err := db.DeleteTask(id)
+			if err != nil {
+				fmt.Printf("Failed to delete \"%d\" task from the list.\n", id)
+			}
+			fmt.Printf("Deleted task \"%d\" from the todo list.\n", id)
+		}
 	},
 }
 
